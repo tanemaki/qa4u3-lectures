2d633448bdc154395ac9d6d9fbaa9a03e0ccbc41
d-prime <tanemaki@users.noreply.github.com>
2025-01-31 19:13:40 +0900

    my_task3タスクを追加し、QUBO行列を生成する機能を実装

--- scripts/lecture01.py
+++ scripts/lecture01.py
@@ -11,3 +11,22 @@ def my_task2(ctx):
     import numpy as np
 
     print(np.arange(10))
+
+
+@invoke.task
+def my_task3(ctx, N: int = 50, seed: int | None = None):
+    """
+    N (int): 変数の数
+    """
+    import numpy as np
+
+    if seed is not None:
+        seed = int(seed)
+        np.random.seed(seed)
+
+    # QUBO（Quadratic Unconstraind Binary Optimization）行列の作成
+    QUBO = np.random.randn(N, N)
+    # 対称行列にするぞー
+    QUBO = (QUBO + QUBO.T) / 2
+
+    print(QUBO)
