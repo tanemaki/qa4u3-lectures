52a6103419d680866f017e565f054d10af52bedb
d-prime <tanemaki@users.noreply.github.com>
2025-02-01 10:25:35 +0900

    乱数シード、読み出し回数、スイープ回数、サンプラー名を取得するウィジェットを追加し、ランダムなQUBO行列を生成する機能を実装

--- app/app.py
+++ app/app.py
@@ -4,6 +4,11 @@ import streamlit as st
 def app():
     st.title("Hello World!")
 
+    # QUBO（Quadratic Unconstraind Binary Optimization）行列の作成
+    QUBO = np.random.randn(N, N)
+    # 対称行列にするぞー
+    QUBO = (QUBO + QUBO.T) / 2
+
 
 if __name__ == "__main__":
     app()
--- app/pages/lecture_1.py
+++ app/pages/lecture_1.py
@@ -1,10 +1,108 @@
+import time
+
 import numpy as np
 import openjij as oj
 import streamlit as st
 
 
+def insert_widget_to_get_seed() -> int | None:
+    use_seed = st.sidebar.checkbox("乱数シードを固定する")
+    if use_seed:
+        seed = st.sidebar.number_input("乱数のシード", 0)
+    else:
+        seed = None
+    return seed
+
+
+def insert_widget_to_get_num_reads():
+    num_reads = st.sidebar.text_input("読み出し回数（試行回数）", value="")
+    if num_reads == "":
+        num_reads = None
+    else:
+        num_reads = int(num_reads)
+        if num_reads <= 0:
+            st.error(f"読み出し回数は1以上の整数で指定してください。`{num_reads = }`")
+            st.stop()
+
+    return num_reads
+
+
+def insert_widget_to_get_num_sweeps():
+    num_sweeps = st.sidebar.text_input("スイープ回数（更新回数）", value="")
+    if num_sweeps == "":
+        num_sweeps = None
+    else:
+        num_sweeps = int(num_sweeps)
+        if num_sweeps <= 0:
+            st.error(f"スイープ回数は1以上の整数で指定してください。`{num_sweeps = }`")
+            st.stop()
+
+    return num_sweeps
+
+
+def insert_widget_to_get_sampler_name():
+    sampler_name = st.sidebar.radio(
+        "サンプラーの名前",
+        ["ojsa", "ojsqa"],
+        captions=["OpenJij's SA sampler", "OpenJij's SQA sampler"],
+    )
+    return sampler_name
+
+
+def make_random_qubo(N: int) -> np.ndarray:
+    """
+    ランダムなQUBO行列を作成する。
+    """
+
+    # QUBO（Quadratic Unconstraind Binary Optimization）行列の作成
+    QUBO = np.random.randn(N, N)
+    # 対称行列にする
+    QUBO = (QUBO + QUBO.T) / 2
+
+    return QUBO
+
+
 def render_page():
     st.title("Hello Lecture 1!")
+
+    seed = insert_widget_to_get_seed()
+    if seed is not None:
+        np.random.seed(seed)
+
+    N = st.sidebar.number_input("スピン変数の数", 1, 100, 5)
+    num_reads = insert_widget_to_get_num_reads()
+    num_sweeps = insert_widget_to_get_num_sweeps()
+    sampler_name = insert_widget_to_get_sampler_name()
+
+    st.write(f"`{N = }`")
+    st.write(f"`{num_reads = }`")
+    st.write(f"`{num_sweeps = }`")
+    st.write(f"`{sampler_name = }`")
+
+    # if st.button("QUBO行列を作成する"):
+    QUBO = make_random_qubo(N)
+    st.write(QUBO)
+
+    if sampler_name == "ojsa":
+        sampler = oj.SASampler()
+    elif sampler_name == "ojsqa":
+        sampler = oj.SQASampler()
+    else:
+        raise ValueError(f"Invalid sampler name: {sampler_name}")
+
+    if num_reads:
+        num_reads = int(num_reads)
+    if num_sweeps:
+        num_sweeps = int(num_sweeps)
+
+        start = time.time()
+        response = sampler.sample_ising(
+            QUBO, num_reads=num_reads, num_sweeps=num_sweeps
+        )
+        elapsed_time = time.time() - start
+
+        st.write(response)
+        st.write(f"Elapsed time: {elapsed_time:.3f} [s]")
 
 
 if __name__ == "__main__":
